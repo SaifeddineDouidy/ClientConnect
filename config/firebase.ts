8367fbc7a8db13d3e2f7fa9b4ec1a59d26c95f3d
JunSaif <121460498+SaifeddineDouidy@users.noreply.github.com>
2025-04-20 16:21:34 +0100

    Update firebase.ts (secrets removed)

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,13 +6,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAaPNX0qrh_zsHgVSXpSBMDcbwsta6cYhc",
-  authDomain: "clientconnect-3107a.firebaseapp.com",
-  projectId: "clientconnect-3107a",
-  storageBucket: "clientconnect-3107a.firebasestorage.app",
-  messagingSenderId: "493375465986",
-  appId: "1:493375465986:web:3ff788dbf576479b6f151b",
-  measurementId: "G-VMDHS02F87"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
 };
 
 // Initialize Firebase
