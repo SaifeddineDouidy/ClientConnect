490fb42e706997f2e6a700c9ad21d287e4b7f058
JunSaif <121460498+SaifeddineDouidy@users.noreply.github.com>
2025-05-04 11:14:12 +0100

    Update firebase.ts
    Hidding secrets

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,13 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDtEzY8t4lbOsQ3gJYvq6GQJBct-opJ7vU",
-  authDomain: "clientconnect-152ee.firebaseapp.com",
-  projectId: "clientconnect-152ee",
-  storageBucket: "clientconnect-152ee.firebasestorage.app",
-  messagingSenderId: "815353874383",
-  appId: "1:815353874383:web:3fa2be81a1b8d9329474ba",
-  measurementId: "G-85ZL9885VH"
+  
 };
 
 // Initialize Firebase
